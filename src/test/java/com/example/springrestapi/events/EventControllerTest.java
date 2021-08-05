0846a20a55f93a559218ef250cadf1936efcab7f
류정문 <rjm9303@gmsolution.co.kr>
2021-08-05 09:20:57 +0900

    Spring Restdocs 적용

--- src/test/java/com/example/springrestapi/events/EventControllerTest.java
+++ src/test/java/com/example/springrestapi/events/EventControllerTest.java
@@ -1,12 +1,15 @@
 package com.example.springrestapi.events;
 
+import com.example.springrestapi.common.RestDocsConfiguration;
 import com.example.springrestapi.common.TestDescription;
 import com.example.springrestapi.mapper.ModernMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -21,6 +24,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
 class EventControllerTest {
 
     @Autowired
